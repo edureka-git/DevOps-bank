807b73d54c9165638749f747ca9b218de11fd465
edureka-git <yeshwanth.p@edureka.co>
2017-03-15 12:33:00 +0530

    Update ValidateLogin.java

--- src/com/edureka/bankservice/ValidateLogin.java
+++ src/com/edureka/bankservice/ValidateLogin.java
@@ -15,7 +15,7 @@ import javax.servlet.http.HttpSession;
 @WebServlet("/ValidateLogin")
 public class ValidateLogin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private static final String FILENAME = "/var/lib/jenkins/workspace/FP_QA_PACKAGE/target/DevopsBank-0.0.1-SNAPSHOT/WEB-INF/classes/Login.txt";
+	private static final String FILENAME = "/var/lib/jenkins/workspace/FP_QA_PACKAGE/src/Login.txt";
     public ValidateLogin() {
         super();
         }
